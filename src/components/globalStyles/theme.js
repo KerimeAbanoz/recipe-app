d85bc1825a57092ee2a47a61d284d976ef248af7
kerime abanoz <krmeabnz@gmail.com>
2022-10-31 22:55:46 +0300

    hamburger added

--- src/components/globalStyles/theme.js
+++ src/components/globalStyles/theme.js
@@ -3,5 +3,10 @@ const theme = {
         navbarBgColor:"#E1F1DD",
         mainColor: "#00ADB5",
         logoColor:"#393e46"
+    },
+    screens:{
+        lg:"902px"
     }
 }
+
+export default theme;
