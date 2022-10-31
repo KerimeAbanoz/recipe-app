2242c175af4d7c1e2b440aac016aed33ed82293f
kerime abanoz <krmeabnz@gmail.com>
2022-10-31 20:21:51 +0300

    started

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./../components/nav/Navbar";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
-  )
-}
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
