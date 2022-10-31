6c80916d8a4b189d5e6b4d2066977a7394e23b5d
kerime abanoz <krmeabnz@gmail.com>
2022-10-31 22:03:51 +0300

    break

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-import LoginContainer from "./Login.style";
+import {
+  LoginContainer,
+  StyledImg,
+  FormContainer,
+  Header,
+  StyledInput,
+  StyledForm,
+  StyledButton,
+} from "./Login.style";
+import { useNavigate } from "react-router-dom";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const userInfo = {
+    username: "Admin",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+
   return (
     <LoginContainer>
-      
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}Reciepe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type="text" />
+          <StyledInput placeholder="Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
     </LoginContainer>
-
-  )
+  );
 };
 
 export default Login;
