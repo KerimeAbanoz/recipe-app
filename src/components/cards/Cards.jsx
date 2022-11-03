78b32b23011d0abe4123c00f0bb587faaa269bef
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 11:41:34 +0300

    recap

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
-import defaultImage from "../../assets/default-image.jpg"
-import {useNavigate} from "react-router-dom"
+import defaultImage from "../../assets/default-image.jpg";
+import { useNavigate } from "react-router-dom";
 const Cards = ({ recipes }) => {
   const navigate = useNavigate();
   return (
@@ -12,9 +12,12 @@ const Cards = ({ recipes }) => {
             <Header>{recipe.label} </Header>
             <Image src={recipe.image || defaultImage} />
             <Button
-            onClick={() =>
-            navigate("detail",{state:recipe, replace:false})}
-            >View More</Button>
+              onClick={() =>
+                navigate("detail", { state: recipe, replace: false })
+              }
+            >
+              View More
+            </Button>
           </Card>
         );
       })}
