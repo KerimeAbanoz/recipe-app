7c4a50dcc5cb3fa25295d27d593ad5c03c4564a2
kerime abanoz <krmeabnz@gmail.com>
2022-12-29 22:49:07 +0300

    some progress

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,23 +8,20 @@ import {
   StyledForm,
   StyledButton,
 } from "./Login.style";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import meal from "../../assets/meal.svg";
 const Login = () => {
- 
   const navigate = useNavigate();
-  
-  const userInfo={
-    username:"admin"
-  }
+
+  const userInfo = {
+    username: "admin",
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    sessionStorage.setItem("user",JSON.stringify(userInfo))
-    navigate(-1)
-  }
-
-
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
 
   return (
     <LoginContainer>
